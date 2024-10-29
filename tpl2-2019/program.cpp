54538f14f0a445e135557d12fe143247caa4342b
Maximo Bar <maximobar2003@gmail.com>
2024-10-29 12:50:10 -0300

    Cambios en el tpl

--- tpl2-2019/program.cpp
+++ tpl2-2019/program.cpp
@@ -26,16 +26,23 @@ void fill_ordprev(tree<int> &T,list<int> &L) {
 }
 
 
-void step_a_lo_ancho(graph& G, tree<char>& T, tree<char>::iterator itTree, list<char>&visitados, graph::iterator itGraph){
+void step_a_lo_ancho(graph& G, tree<char>& T, tree<char>::iterator itTree, list<char>&visitados, graph::iterator itGraph, tree<char>::iterator itPadre, list<char>L){
   if(itGraph == G.end()) return;
+  if(itTree != T.begin()){
+    for(auto elem : itGraph -> second){
+      if(elem == *itPadre /*and find(L.begin(), L.end(), elem) != L.end()*/) return;
+    }
+  }
   if(find(visitados.begin(), visitados.end(), itGraph -> first) != visitados.end()) return;
   auto it = itTree.lchild();
   while(it != T.end()) it++;
-  itTree = T.insert(it, itGraph -> first);
+  it = T.insert(it, itGraph -> first);
   visitados.push_back(itGraph -> first);
   auto itList = (itGraph -> second).begin();
   while (itList != (itGraph -> second).end()){
-    step_a_lo_ancho(G, T, itTree, visitados, G.find(*itList));
+    auto newList = itGraph -> second;
+    newList.remove(*itList);
+    step_a_lo_ancho(G, T, it, visitados, G.find(*itList), itTree, newList);
     itList++;
   }
 }
@@ -44,13 +51,29 @@ void a_lo_ancho(graph& G, tree<char>& T){
   auto it = G.begin();
   list<char>visitados;
   T.insert(T.begin(), it -> first);
+  visitados.push_back(it -> first);
   auto itList = (it -> second).begin();
   while (itList != (it -> second).end()){
-    step_a_lo_ancho(G, T, T.begin(),visitados, G.find(*itList));
+    step_a_lo_ancho(G, T, T.begin(),visitados, G.find(*itList), T.begin(), visitados);
     itList++;
   }
 }
 
+//void step_a_lo_ancho(graph& G, graph::iterator itGraph, tree<char>& T, tree<char>::iterator itTree, list<char>&visitados){
+//  if(itGraph == G.end()) return;
+//  if(find(visitados.begin(), visitados.end(), itGraph -> first) != visitados.end());
+//  itTree = T.insert(itTree, itGraph -> first);
+//  visitados.push_back(itGraph -> first);
+//  itTree = itTree.lchild();
+//  while(itTree != T.end()) itTree++;
+//  auto it = (itGraph -> second).begin();
+//  while(it != (itGraph -> second).end())step_a_lo_ancho(G, G.find(*(it++)), T, itTree, visitados);
+//}
+//  
+//void a_lo_ancho(graph& G, tree<char>& T){
+//    list<char>visitados;
+//    step_a_lo_ancho(G, G.begin(), T, T.begin(), visitados);
+//}
 void intersect_map(map<int,list<int>> &A, map<int,list<int>> &B,map<int,list<int>> &C){
   // COMPLETAR AQUI...
 }
